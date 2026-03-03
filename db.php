3be002fee51542d12cb50538335a36620ececcca
jplazos560279-lang <j.plazos.560279@umindanao.edu.ph>
2026-03-04 00:35:36 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,4 @@
 <?php
-error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING); // hide notices & warnings
-ini_set('display_errors', 1);
-
 $conn = new mysqli("localhost", "root", "", "student_record_system_db");
 
 if ($conn->connect_error) {
@@ -9,4 +6,5 @@ if ($conn->connect_error) {
 }
 
 session_start();
+
 ?>
